5c71c431d081384ffc556c7b9d853efc564ffe7a
Joshua Ignacio <joshua@bld.ai>
2023-04-23 13:08:51 +0800

    I added a new command to support using web search, using pyautogui, from an existing Google Chrome window.

---
+++ whispering_assistant/commands/web_search.py
@@ -0,0 +1,26 @@
+import time
+import pyperclip
+from whispering_assistant.commands.command_base_template import BaseCommand, command_types
+
+
+class WebSearch(BaseCommand):
+    trigger = "search the web"
+    command_type = command_types['CHAINABLE_LONG']
+    keywords = {
+        "action": ["search", "ask", "find"], "subject": ["google", "web"]}
+
+    def run(self, text_parameter, raw_text, *args, **kwargs):
+        import pyautogui
+        pyautogui.hotkey('ctrl', 'alt', 'shift', 'w', interval=0.1)
+
+        # Type the string
+        old_clipboard = pyperclip.paste()
+        pyperclip.copy(text_parameter.lstrip())
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 't')
+        time.sleep(0.2)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.2)
+        pyautogui.press('enter')
+        pyperclip.copy(old_clipboard)
+        time.sleep(0.5)
