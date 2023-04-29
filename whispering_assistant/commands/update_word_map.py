d9b3e73ae6a484728ba274003429abe47f391b38
Joshua Ignacio <joshua@bld.ai>
2023-04-30 00:30:58 +0800

    Added a new plugin to easily update the accuracy of transcription using word maps

---
+++ whispering_assistant/commands/update_word_map.py
@@ -0,0 +1,13 @@
+from whispering_assistant.commands.command_base_template import BaseCommand, command_types
+import subprocess
+
+
+class UpdateWordMap(BaseCommand):
+    trigger = "update_word_map"
+    command_type = command_types['ONE_SHOT']
+    keywords = {
+        "action": ["update", "change"], "subject": ["word map", "wordmap"]}
+
+    def run(self, text_parameter, raw_text, *args, **kwargs):
+        file_path = "/home/joshua/extrafiles/projects/WhisperingAssistant/whispering_assistant/assets/docs/prompt.yml"
+        subprocess.call(['atom', file_path])
