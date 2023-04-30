d408126ff3abe7ac184da57b0ff7bfcf945c2be4
Joshua Ignacio <joshua@bld.ai>
2023-04-30 10:17:54 +0800

    Only do the object detection if it's necessary, like if the active window is expected to have the object that you are trying to find.

--- whispering_assistant/commands/type_input.py
+++ whispering_assistant/commands/type_input.py
@@ -1,54 +1,48 @@
 from whispering_assistant.commands.command_base_template import BaseCommand, FALL_BACK_COMMAND
 import pyperclip
 import time
-import subprocess
+
+from whispering_assistant.utils.window_dialogs import get_active_window_name
 
 x2, y2, width2, height2 = 0, 540, 1920, 1080
 region2 = (x2, y2, width2, height2)
 image2 = '/home/joshua/extrafiles/projects/WhisperingAssistant/whispering_assistant/assets/images/CHAT_BOX.png'
 
+
 def paste_input():
-    import pyautogui
+    active_window_name = get_active_window_name()
+    print("active_window_name", active_window_name)
 
-    curr_clipboard = pyperclip.paste()
-    # Look for image2 on the screen
-    image2_location = pyautogui.locateOnScreen(image2, region=region2, confidence=0.8)
+    if 'google chrome' in active_window_name.lower():
+        import pyautogui
+        curr_clipboard = pyperclip.paste()
+        # Look for image2 on the screen
+        image2_location = pyautogui.locateOnScreen(image2, region=region2, confidence=0.8)
 
-    if image2_location is not None:
-        print(f"Image 2 chat box found at {image2_location}")
+        if image2_location is not None:
+            print(f"Image 2 chat box found at {image2_location}")
 
-        # Click on the center of image2
-        image2_center = pyautogui.center(image2_location)
-        pyautogui.click(image2_center)
+            # Click on the center of image2
+            image2_center = pyautogui.center(image2_location)
+            pyautogui.click(image2_center)
 
-        # Type the string
-        pyautogui.typewrite(curr_clipboard)
-        time.sleep(0.5)
+            # Type the string
+            pyautogui.typewrite(curr_clipboard)
+            time.sleep(0.5)
 
-        # Press the enter key
-        pyautogui.press('enter')
-        print("Typing and pressing enter done.")
+            # Press the enter key
+            pyautogui.press('enter')
+            print("Typing and pressing enter done.")
+        else:
+            print("Image 2 chat box not found on the screen.")
+            # Simulate pressing Ctrl+V
+
+            pyautogui.hotkey('ctrl', 'v')
     else:
         print("Image 2 chat box not found on the screen.")
         # Simulate pressing Ctrl+V
-
+        import pyautogui
         pyautogui.hotkey('ctrl', 'v')
-
-        # Get the active window's name
-        active_window_name = get_active_window_name()
-
-        print(active_window_name)
-
-        # Check if the active window's name contains "Google Chrome" or "Atom"
-        if "Atom" in active_window_name:
-            # Simulate pressing the Return key
-            pyautogui.press('enter')
-
-
-def get_active_window_name():
-    cmd = "xdotool getactivewindow getwindowname"
-    window_name = subprocess.check_output(cmd, shell=True).decode().strip()
-    return window_name
 
 
 class TypeInput(BaseCommand):
@@ -57,7 +51,7 @@ class TypeInput(BaseCommand):
     def run(self, *args, text_parameter="", **kwargs):
         old_clipboard = pyperclip.paste().lstrip()
         pyperclip.copy(text_parameter.lstrip())
-        time.sleep(0.5)
+        time.sleep(0.3)
         paste_input()
         time.sleep(0.3)
         pyperclip.copy(old_clipboard)
--- whispering_assistant/utils/window_dialogs.py
+++ whispering_assistant/utils/window_dialogs.py
@@ -1,20 +1,26 @@
-import subprocess
-import signal
 from Xlib import X, display
 
-def generate_info_dialog(text):
-    transcribing_window = subprocess.Popen(
-        ['zenity', '--info', '--title', text, '--text', text])
-    return transcribing_window
-
-def kill_dialog(window_inst):
-    return window_inst.send_signal(signal.SIGTERM)
 
 def get_active_window_id():
     d = display.Display()
     root = d.screen().root
     window_id = root.get_full_property(d.intern_atom('_NET_ACTIVE_WINDOW'), X.AnyPropertyType).value[0]
     return window_id
+
+
+def get_active_window_name():
+    d = display.Display()
+    root = d.screen().root
+    window_id = root.get_full_property(d.intern_atom('_NET_ACTIVE_WINDOW'), X.AnyPropertyType).value[0]
+    window = d.create_resource_object('window', window_id)
+
+    try:
+        window_name = window.get_full_property(d.intern_atom('_NET_WM_NAME'), X.AnyPropertyType).value.decode("utf-8")
+    except AttributeError:
+        window_name = window.get_wm_name()
+
+    return window_name
+
 
 def activate_window(window_id):
     d = display.Display()
