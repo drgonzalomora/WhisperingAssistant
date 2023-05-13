7480fbe363664c53b287d43fb9dfdc3592e3ddc1
Joshua Ignacio <joshua@bld.ai>
2023-05-13 20:43:19 +0800

    Added a plugin, a utility function to easily get the intent of a command based on the generated embeddings.

---
+++ whispering_assistant/utils/command_intent_detection.py
@@ -0,0 +1,73 @@
+import os
+
+from whispering_assistant.commands import COMMAND_PLUGINS
+from whispering_assistant.utils.semantic_search_index import generate_index_csv, search_index_csv
+from whispering_assistant.utils.vector_embeddings_storage import init_faiss_index
+
+default_intent_index = "/home/joshua/extrafiles/projects/WhisperingAssistant/whispering_assistant/assets/docs/plugin_commands_intents.csv"
+faiss_index_file_name = "/home/joshua/extrafiles/projects/WhisperingAssistant/whispering_assistant/assets/docs/faiss_index_intents.idx"
+
+query_instruction = 'Represent the command intent for retrieving similar examples: '
+storing_instruction = 'Represent the command intent examples for retrieval: '
+
+faiss_index, save_faiss_index = init_faiss_index(faiss_index_file_name)
+
+# 📌 TODO: Add a logic to make sure that we need to create the index if it's not yet existing instead of manually running it and assuming that the index is already created.
+
+def parse_plugin_commands_examples():
+    result = []
+    for plugin in COMMAND_PLUGINS.values():
+        if plugin and hasattr(plugin, 'examples'):
+            for example in plugin.examples:
+                result.append([plugin.trigger, example])
+    return result
+
+
+intent_list = parse_plugin_commands_examples()
+
+
+def generate_index_for_intent_detection():
+    global intent_list
+    intent_list = parse_plugin_commands_examples()
+    print(intent_list)
+
+    for idx, intent_item in enumerate(intent_list):
+        id_text_with_index = f"{intent_item[0]}_{idx}"
+        generate_index_csv(input_text=intent_item[1], id_text=id_text_with_index, file_name=default_intent_index,
+                           faiss_index=faiss_index, save_faiss_index=save_faiss_index,
+                           storing_instruction=storing_instruction)
+
+
+def get_intent_from_text(command_text):
+    global intent_list
+
+    if not command_text:
+        return None, None
+
+    if not os.path.exists(default_intent_index):
+        generate_index_for_intent_detection()
+
+    top_result, _ = search_index_csv(command_text, n=1, file_name=default_intent_index, faiss_index=faiss_index,
+                                     query_instruction=query_instruction)
+
+    if not top_result:
+        return None, None
+
+    top_result_details = None
+
+    for item in intent_list:
+        if item and top_result and item[0] in top_result['id_text']:
+            top_result_details = item
+            break
+
+    if not top_result_details:
+        return None, None
+
+    print("top_result_prompt", top_result_details[0])
+
+    return top_result_details[0], top_result_details
+
+
+# parse_plugin_commands_examples()
+# generate_index_for_intent_detection()
+# get_intent_from_text('shutdown')
