a6a956da12e584af316e33358d6b7e96adae18fa
Joshua Ignacio <joshua@bld.ai>
2023-05-19 09:24:02 +0800

    I updated the logic for printing the performance measure just to easily see on the logs which part of the code is taking a long time.

--- whispering_assistant/utils/performance.py
+++ whispering_assistant/utils/performance.py
@@ -8,7 +8,11 @@ import functools
 def print_time_profile(start_time, desc):
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"The {desc} execution time is {execution_time} seconds.")
+
+    if execution_time > 0.1:
+        print(f"⚡⚡⚡ ⏰ The {desc} execution time is {execution_time} seconds.")
+    else:
+        print(f"⚡⚡⚡ The {desc} execution time is {execution_time} seconds.")
 
 
 """
