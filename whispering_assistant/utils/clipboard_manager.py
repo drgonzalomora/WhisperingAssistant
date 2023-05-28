a698f29bf45400be929ef105a60776f1c1a66719
Joshua Ignacio <joshua@bld.ai>
2023-05-28 08:34:09 +0800

    I updated the code such that the fallback type input function will be faster at pasting the text since it's already moved in a separate thread.

--- whispering_assistant/utils/clipboard_manager.py
+++ whispering_assistant/utils/clipboard_manager.py
@@ -5,6 +5,7 @@ import pyperclip as pyclip
 # Try to force pyperclip to use a different clipboard backend:
 pyclip.set_clipboard('xclip')
 
+
 class ClipboardHandler:
     def __init__(self):
         self.clipboard_content = ""
@@ -29,3 +30,33 @@ class ClipboardHandler:
             clipboard_thread.start()
         except Exception as e:
             print(f"An error occurred while handling the clipboard: {e}")
+
+
+class TypingViaClipBoardHandler:
+    def __init__(self):
+        self.old_clipboard = ""
+
+    def load_clipboard(self, text_parameter):
+        try:
+            self.old_clipboard = pyclip.paste()
+            self.old_clipboard = self.old_clipboard.lstrip() if self.old_clipboard is not None else ""
+        except Exception as e:
+            self.old_clipboard = ""
+            print(f"Error occurred while accessing clipboard: {e}")
+
+        pyclip.copy(text_parameter.lstrip())
+
+    def paste_input(self):
+        print("Paste input")
+        import pyautogui
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.1)
+        pyclip.copy(self.old_clipboard)
+
+    def run_thread(self, text_parameter):
+        def copy_paste_restore():
+            self.load_clipboard(text_parameter)
+            self.paste_input()
+
+        clipboard_thread = threading.Thread(target=copy_paste_restore)
+        clipboard_thread.start()
